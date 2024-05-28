10fc212656b5dcd58b818c2ae25eb74b754840bb
VanNghia <nghiadauvan@gmail.com>
2024-05-28 10:42:05 +0700

    firt app

---
+++ src/main/java/org/example/dauvannghia_test/DauVanNghiaTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.dauvannghia_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DauVanNghiaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DauVanNghiaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dauvannghia_test/DauVanNghiaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dauvannghia_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DauVanNghiaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
